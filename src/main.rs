ed8a8e140a0fc480a1d4f2ff31172a8a995a8399
Julian <hahnjulian@hotmail.de>
2017-04-30 21:27:16 +0200

    Edited but still problems with the array

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 use std::io;
 
 fn main() {
-    let mut count = 0;
+    let mut count = 0;//Initial count declaration so that it doesn't fall back to 0
     loop{
 //    	let in = read_var('Gegenstand');
+///////////////////////////////Alternative input:////////////////////////////////////////////
 	println!("Please type an item:");
-	let mut in = String::new();
+	let mut stuff = String::new();
 	
-	io::stdin().readline(&mut in)
+	io::stdin().read_line(&mut stuff)
 		.expect("Failed to read line");
-//Hier fängt der alte Teil wieder an
-        println!("Die Eingabe war: {}",in);
-	putinlist(in);
-	println!("Die Liste sieht nun wiefolgt aus: {}", list);
-	count = count+1;
+	let mut list = [&str; 20];//still false type
+///////////////////////////////Output part////////////////////////////////////////////////////
+	println!("Die Eingabe war: {}",stuff);
+	list[count] = stuff;
+
+	println!("Die Liste sieht nun wiefolgt aus: {}", list);//print of the full array (all items)
+	count = count+1;//Increase of count and with it the place in the array
     }
 }
 
+//outsourced input
 fn read_var (c:String){
     println!("Please type {}", c); 
     let mut v = String::new();
@@ -26,6 +30,7 @@ fn read_var (c:String){
 
 }
 
-fn putinlist(in: String) -> [&str; 80]{
-
-}
+///////////ToDo://///////////
+//- find right input type
+//- right output (print the full array)
+//- variable if you want to input/output/both/exit
